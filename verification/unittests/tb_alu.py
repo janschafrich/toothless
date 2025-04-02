1110fd758c4739fd28feefc3255cfd0a1b6a7830
Jan-Eric Schäfrich <j.schaefrich@tum.de>
2025-04-02 21:05:00 +0200

    updated testbench to match current RTL file

--- verification/unittests/tb_alu.py
+++ verification/unittests/tb_alu.py
@@ -8,35 +8,15 @@ import numpy as np
 CLK_PRD = 10
 
 
-async def generate_clock(dut):
-    """ Generate clock signal """
-    
-    for cycle in range(100):
-        dut.clk.value = 0
-        await Timer(0.5*CLK_PRD, units='ns')  # suspend execution
-        dut.clk.value = 1
-        await Timer(0.5*CLK_PRD, units='ns')
-
-
 @cocotb.test()
 async def test_alu(dut):
     """ ALU test cases """
 
-    await cocotb.start(generate_clock(dut))     # run in background/parallel
-
-    dut.rst_n.value         = 0
     dut.operator_i.value    = 0
     dut.operand_a_i.value   = 0
     dut.operand_b_i.value   = 0
     dut.result_o.value      = 0
 
-
-
-    print("Test reset")
-    
-    await Timer(3*CLK_PRD, units='ns')
-    dut.rst_n.value = 1                              # release reset
-    assert dut.result_o.value == 0, f"Expected result_o=0, got {hex(dut.result_o.value)}"
 
     # testing alu operations
 
